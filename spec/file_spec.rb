3185a94fff0dbc520f0dd852363930c42933ccc4
Nick Veys <nick@codelever.com>
2013-11-06 08:20:07 -0600

    Adding spec for GPS log file

--- spec/file_spec.rb
+++ spec/file_spec.rb
@@ -52,6 +52,56 @@ describe Jeti::Log::File do
 
   end
 
+  context 'with data file gps-crash.log' do
+
+    before(:all) { @file = Jeti::Log::File.new(data_file('gps-crash.log')) }
+
+    subject { @file }
+
+    its(:duration) { should be_within(0.1).of(286.2) }
+
+    its(:antenna1_signals?) { should be_true }
+
+    it { should have(553).antenna1_signals }
+
+    it 'should have some select antenna1 signals' do
+      expect(subject.antenna1_signals[0]).to eql([219714, 9])
+      expect(subject.antenna1_signals[50]).to eql([244741, 9])
+      expect(subject.antenna1_signals[100]).to eql([270738, 9])
+    end
+
+    its(:antenna2_signals?) { should be_true }
+
+    it { should have(553).antenna2_signals }
+
+    it 'should have some select antenna2 signals' do
+      expect(subject.antenna1_signals[10]).to eql([223938, 9])
+      expect(subject.antenna1_signals[60]).to eql([249938, 9])
+      expect(subject.antenna1_signals[90]).to eql([265538, 9])
+    end
+
+    its(:rx_voltages?) { should be_true }
+
+    it { should have(553).rx_voltages }
+
+    it 'should have some select rx voltages' do
+      expect(subject.rx_voltages[10]).to eql([223938, 4.69])
+      expect(subject.rx_voltages[60]).to eql([249938, 4.69])
+      expect(subject.rx_voltages[90]).to eql([265538, 4.69])
+    end
+
+    its(:signal_qualities?) { should be_true }
+
+    it { should have(553).signal_qualities }
+
+    it 'should have some select signal qualities' do
+      expect(subject.signal_qualities[10]).to eql([223938, 100])
+      expect(subject.signal_qualities[60]).to eql([249938, 100])
+      expect(subject.signal_qualities[90]).to eql([265538, 100])
+    end
+
+  end
+
   it 'should raise for invalid or missing files' do
     files = invalid_data_files
     files.should have(9).files
