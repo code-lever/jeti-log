72bcf83a770af087ac452876fd3f94fcf7af0270
Nick Veys <nickveys@gmail.com>
2013-11-11 14:48:56 -0600

    Adding MezonData spec

--- spec/mezon_data_spec.rb
+++ spec/mezon_data_spec.rb
@@ -2,6 +2,30 @@ require 'spec_helper'
 
 describe Jeti::Log::MezonData do
 
-  it 'does stuff'
+  subject { Jeti::Log::MezonData.new(25, 15.1, 5, 5.4, 1, 250, 1000, 25, 150, 99) }
+
+  its(:time) { should eql(25) }
+
+  its(:battery_voltage) { should be_within(0.1).of(15.1) }
+
+  its(:battery_current) { should be_within(0.1).of(5) }
+
+  its(:bec_voltage) { should be_within(0.1).of(5.4) }
+
+  its(:bec_current) { should be_within(0.1).of(1) }
+
+  its(:capacity) { should eql(250) }
+
+  its(:rpm) { should eql(1000) }
+
+  its(:temperature) { should be_within(0.1).of(25) }
+
+  it 'should convert temperature to F' do
+    expect(subject.temperature(:f)).to be_within(0.1).of(77)
+  end
+
+  its(:run_time) { should eql(150) }
+
+  its(:pwm) { should eql(99) }
 
 end
