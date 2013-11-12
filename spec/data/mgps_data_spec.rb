9fe290ddc2d4c3d9882c79fe3c1f1f2b19e16a14
Nick Veys <nick@codelever.com>
2013-11-11 20:29:58 -0600

    Adding MGPS spec

--- spec/data/mgps_data_spec.rb
+++ spec/data/mgps_data_spec.rb
@@ -2,6 +2,20 @@ require 'spec_helper'
 
 describe Jeti::Log::Data::MGPSData do
 
-  it 'does stuff'
+  subject { described_class.new(25, [41.124, -95.024, 350, 45]) }
+
+  its(:time) { should eql(25) }
+
+  its(:latitude) { should be_within(0.001).of(41.124) }
+
+  its(:longitude) { should be_within(0.001).of(-95.024) }
+
+  its(:altitude) { should be_within(0.01).of(114.83) }
+
+  it 'should provide altitude to meters' do
+    expect(subject.altitude(:meters)).to be_within(0.01).of(350)
+  end
+
+  its(:course) { should eql(45) }
 
 end
